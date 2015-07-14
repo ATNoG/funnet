7cffb249e195757e414ddcd9d6fc528d4b1e6191
Mario Antunes <mariolpantunes@gmail.com>
2015-07-14 02:00:33 +0100

    fix tarefa06

--- tarefa06/src/pt/it/av/atnog/funnet/tarefa06/map/HandlerMapDisplay.java
+++ tarefa06/src/pt/it/av/atnog/funnet/tarefa06/map/HandlerMapDisplay.java
@@ -30,29 +30,6 @@ public class HandlerMapDisplay extends Handler {
     }
 
     public void handleMessage(Message message) {
-        if (firstTime) {
-            LatLng c = db.selfPos();
-            if (c != null) {
-                map.animateCamera(CameraUpdateFactory.newLatLngZoom(c, 18.0f));
-                markers.add(map.addMarker(new MarkerOptions()
-                        .position(db.selfPos())
-                        .title(db.selfName())
-                        .icon(BitmapDescriptorFactory.fromBitmap(db.selfIcon()))));
-                firstTime = false;
-            }
-        } else {
 
-            for (Marker m : markers)
-                m.remove();
-            markers.clear();
-
-            List<UsersDB.User> users = db.getUsers();
-            for (UsersDB.User user : users) {
-                markers.add(map.addMarker(new MarkerOptions()
-                        .position(user.latlng)
-                        .title(user.name)
-                        .icon(BitmapDescriptorFactory.fromBitmap(db.selfIcon()))));
-            }
-        }
     }
 }
